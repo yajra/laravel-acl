ccb6e4d294195c9897a7d9d2198171e1d9fb1569
Arjay Angeles <aqangeles@gmail.com>
2016-03-29 10:53:51 +0800

    Fix model docblock path.

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -6,7 +6,7 @@ use Yajra\Acl\Traits\HasPermission;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property \Yajra\Acl\Permission permissions
+ * @property \Yajra\Acl\Models\Permission permissions
  * @property bool system
  */
 class Role extends Model
