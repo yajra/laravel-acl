b689c6f9dae6e8967d7c28bcc4105613f61ae773
Arjay Angeles <aqangeles@gmail.com>
2016-06-09 00:06:06 +0800

    Add docs explaining acl config file.

--- config/acl.php
+++ config/acl.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    /**
+     * Role class used for ACL.
+     */
     'role'       => \Yajra\Acl\Models\Role::class,
+
+    /**
+     * Permission class used for ACL.
+     */
     'permission' => \Yajra\Acl\Models\Permission::class,
 ];
