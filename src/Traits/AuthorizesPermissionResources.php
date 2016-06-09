d625d19cde05c6b7b3a9cd92f3417b4326fcefc5
Arjay Angeles <aqangeles@gmail.com>
2016-06-09 08:26:38 +0800

    Use different name to avoid conflict with laravel core resource authorization.

--- src/Traits/AuthorizesPermissionResources.php
+++ src/Traits/AuthorizesPermissionResources.php
@@ -2,14 +2,14 @@
 
 namespace Yajra\Acl\Traits;
 
-trait AuthorizesResources
+trait AuthorizesPermissionResources
 {
     /**
      * Permission resource ability mapping.
      *
      * @var array
      */
-    protected $resourceAbilityMap = [
+    protected $resourcePermissionMap = [
         'index'   => 'lists',
         'create'  => 'create',
         'store'   => 'create',
@@ -20,25 +20,25 @@ trait AuthorizesResources
     ];
 
     /**
-     * Controller specific ability map.
+     * Controller specific permission ability map.
      *
      * @var array
      */
-    protected $customAbilityMap = [];
+    protected $customPermissionMap = [];
 
     /**
-     * Authorize a resource action based on the incoming request.
+     * Authorize a permission resource action based on the incoming request.
      *
      * @param  string $model
      * @param  string|null $parameter
      * @param  array $options
      * @return void
      */
-    public function authorizeResource($model, $parameter = null, array $options = [])
+    public function authorizePermissionResource($model, $parameter = null, array $options = [])
     {
         $parameter = $parameter ?: strtolower(class_basename($model));
 
-        foreach ($this->resourceAbilityMap() as $method => $ability) {
+        foreach ($this->resourcePermissionMap() as $method => $ability) {
             $modelName = in_array($method, ['index', 'create', 'store']) ? $model : $parameter;
 
             $this->middleware("can:{$modelName}.{$ability}", $options)->only($method);
@@ -46,12 +46,12 @@ trait AuthorizesResources
     }
 
     /**
-     * Get the map of resource methods to ability names.
+     * Get the map of permission resource methods to ability names.
      *
      * @return array
      */
-    protected function resourceAbilityMap()
+    protected function resourcePermissionMap()
     {
-        return array_merge($this->resourceAbilityMap, $this->customAbilityMap);
+        return array_merge($this->resourcePermissionMap, $this->customPermissionMap);
     }
 }
