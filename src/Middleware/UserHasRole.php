55004a3435c7eb98c7be015f3a9dc6fd8a3140f8
Arjay Angeles <aqangeles@gmail.com>
2016-11-18 15:02:24 +0800

    Add UserHasRole middleware.

---
+++ src/Middleware/UserHasRole.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Yajra\Acl\Middleware;
+
+use Closure;
+
+class UserHasRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (! $request->user() || ! $request->user()->isRole($role)) {
+            if ($request->ajax()) {
+                return response()->json([
+                    'error' => [
+                        'status_code' => 401,
+                        'code'        => 'INSUFFICIENT_PERMISSIONS',
+                        'description' => 'You are not authorized to access this resource.',
+                    ],
+                ], 401);
+            }
+
+            return abort(401, 'You are not authorized to access this resource.');
+        }
+
+        return $next($request);
+    }
+}
