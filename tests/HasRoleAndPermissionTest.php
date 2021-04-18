58373cccbe2c2a8b7e80eb78ac4657bb0c74847b
Arjay Q. Angeles <aqangeles@gmail.com>
2021-04-18 15:02:59 +0800

    Add more test for HasRoleAndPermission trait.

--- tests/HasRoleAndPermissionTest.php
+++ tests/HasRoleAndPermissionTest.php
@@ -4,6 +4,8 @@ namespace Yajra\Acl\Tests;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Yajra\Acl\Models\Permission;
 use Yajra\Acl\Tests\Models\UserWithPermission;
 
 class HasRoleAndPermissionTest extends TestCase
@@ -11,13 +13,9 @@ class HasRoleAndPermissionTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function is_can_assign_permissions_directly_to_user()
+    public function is_can_authorize_user_with_permission()
     {
-        /** @var UserWithPermission $user */
-        $user = UserWithPermission::forceCreate([
-            'name' => 'User with Permissions',
-            'email' => 'user-permission@example.com',
-        ]);
+        $user = $this->createUserWithHasRoleAndPermission();
 
         Auth::login($user);
 
@@ -28,5 +26,83 @@ class HasRoleAndPermissionTest extends TestCase
 
         $this->assertTrue($user->can('article.create'));
         $this->assertTrue(auth()->user()->can('article.create'));
+    }
+
+    /**
+     * @param  string|null  $user
+     * @return \Yajra\Acl\Tests\Models\UserWithPermission
+     */
+    protected function createUserWithHasRoleAndPermission($user = null): UserWithPermission
+    {
+        $user = $user ?: Str::random(10);
+
+        return UserWithPermission::create([
+            'name' => Str::title($user),
+            'email' => $user.'@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_grant_permission_to_user()
+    {
+        $user = $this->createUserWithHasRoleAndPermission();
+
+        $this->assertCount(0, $user->permissions);
+
+        $user->grantPermission($this->createPermission('Test'));
+        $this->assertCount(1, $user->permissions);
+
+        $user->grantPermissionBySlug('article.create');
+        $this->assertCount(2, $user->permissions);
+
+        Permission::createResource('Posts');
+        $user->grantPermissionByResource('Posts');
+        $this->assertCount(7, $user->permissions);
+    }
+
+    /** @test */
+    public function it_can_revoke_user_permission()
+    {
+        $user = $this->createUserWithHasRoleAndPermission();
+        Permission::createResource('Posts');
+        $user->grantPermissionByResource('Posts');
+        $this->assertCount(5, $user->permissions);
+
+        $user->revokePermission(Permission::findBySlug('posts.create'));
+        $this->assertCount(4, $user->permissions);
+
+        $user->revokePermissionBySlug('posts.view');
+        $this->assertCount(3, $user->permissions);
+
+        $user->revokePermissionByResource('Posts');
+        $this->assertCount(0, $user->permissions);
+
+        Permission::createResource('Users');
+        $user->grantPermissionByResource('Users');
+        $this->assertCount(5, $user->permissions);
+        $user->revokeAllPermissions();
+        $this->assertCount(0, $user->permissions);
+    }
+
+    /** @test */
+    public function it_can_return_combined_permissions_from_role_permission_and_user_permission()
+    {
+        $user = $this->createUserWithHasRoleAndPermission();
+        $managerRole = $this->createRole('Manager');
+        $supportRole = $this->createRole('Support');
+        $user->attachRole($managerRole);
+
+        Permission::createResource('Users');
+        Permission::createResource('Posts');
+
+        $managerRole->grantPermissionByResource('Users');
+        $this->assertCount(5, $user->getPermissions());
+
+        $user->grantPermissionByResource('Posts');
+        $this->assertCount(10, $user->getPermissions());
+
+        $user->attachRole($supportRole);
+        $supportRole->grantPermissionByResource('Users');
+        $this->assertCount(10, $user->getPermissions());
     }
 }
